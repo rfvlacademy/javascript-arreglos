c9fa12ab459e0989a932701e56adda949bdb4474
Rafael <rfalconi@vlacademy.com>
2022-09-22 20:39:05 -0500

    filtros de arreglos

--- clase3.js
+++ clase3.js
@@ -1,10 +1,4 @@
-let numbers = [-5, 10, 6, 7, 9];
-
-for (let number in numbers) {
-    console.log(number);
-}
-
-numbers.forEach(number => console.log(number));
+let numbers = [-5, 10, 8, 7, 9];
 
 function deMenorAMayor(arreglo) {
     for (let i = 0; i < arreglo.length; i++) {
@@ -13,12 +7,40 @@ function deMenorAMayor(arreglo) {
             if (arreglo[j] < arreglo[i]) {
                 arreglo[i] = arreglo[j]
                 arreglo[j] = help;
+                help = arreglo[i];
             }
         }
     }
     return arreglo;
 }
+
+function deMayorAMeno(arreglo) {
+    for (let i = 0; i < arreglo.length; i++) {
+        let help = arreglo[i];
+        for (let j = i + 1; j < arreglo.length; j++) {
+            if (arreglo[j] > arreglo[i]) {
+                arreglo[i] = arreglo[j]
+                arreglo[j] = help;
+                help = arreglo[i];
+            }
+        }
+    }
+    return arreglo;
+}
+
 console.log('-----------')
 let numberMenorAMayor = deMenorAMayor(numbers)
 console.log(numberMenorAMayor);
 numberMenorAMayor.forEach(number => console.log(number));
+console.log('-----------')
+let MayorAMeno = deMayorAMeno(numbers)
+console.log(MayorAMeno);
+
+/** filtros en arreglos*/
+
+let users = [{name: 'rfalconi', email: 'r@g.com'}, {name: 'kperez', email: 'k@gm.com'}];
+let userFilter = users.filter(user => user.email === 'r@g.com' && user.name=='rfalconi');
+console.log(userFilter);
+console.log(numbers.filter(number=> number%2!==0));
+console.log(numbers.filter(number=> number%2==0));
+
